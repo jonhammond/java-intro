ca406719e5fc1721451dff84fb123015b46404e1
Jon Hammond <jonh1016@gmail.com>
2016-05-11 10:30:41 -0600

    Initial commit

---
+++ Hello.java
@@ -0,0 +1,7 @@
+public class Hello {
+  public static void main(String [] args) {
+    System.out.println("Hello, Java!");
+    System.out.println(1 + 1);
+    System.out.println("Hello!" + 555);
+  }
+}
